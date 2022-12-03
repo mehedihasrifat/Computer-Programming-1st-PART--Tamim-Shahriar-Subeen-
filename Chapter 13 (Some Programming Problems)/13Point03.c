8836f813de0fb3a93c6de4eb0fb39372971f4e8d
mehedihasrifat <ebd.rifat101@gmail.com>
2022-12-03 13:09:02 +0600

    Uncompleted Program (13Point03.c)

---
+++ Chapter 13 (Some Programming Problems)/13Point03.c
@@ -0,0 +1,28 @@
+/**
+ * @file 13Point03.c
+ * @author Mehedi Hasan Rifat (mehedihasrifat@gmail.com)
+ * @brief This program prints a pattern with the 'C' character
+ * @version 0.1
+ * @date 2022-12-03
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+/*
+    CCCCCCC
+     CCCCCC
+      CCCCC
+       CCCC
+        CCC
+         CC
+          C
+         CC
+        CCC
+       CCCC
+      CCCCC
+     CCCCCC
+    CCCCCCC
+*/
+
+// Code's coming soon...
