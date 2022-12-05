19d9b1162175415dc4d0b8bcefbb2c63d7572689
mehedihasrifat <ebd.rifat101@gmail.com>
2022-12-05 11:54:12 +0600

    2nd commit is added with success

--- Chapter 13 (Some Programming Problems)/13Point03.c
+++ Chapter 13 (Some Programming Problems)/13Point03.c
@@ -3,7 +3,7 @@
  * @author Mehedi Hasan Rifat (mehedihasrifat@gmail.com)
  * @brief This program prints a pattern with the 'C' character
  * @version 0.1
- * @date 2022-12-03
+ * @date 2022-12-04
  *
  * @copyright Copyright (c) 2022
  *
@@ -25,4 +25,48 @@
     CCCCCCC
 */
 
-// Code's coming soon...
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (int i = (num * 2) - 1, j = num, r = num, s = num - 1, m = 2; i >= 1; i--, j--, r--)
+    {
+        if (i >= num)
+        {
+            for (int p = num; p > r; p--)
+            {
+                printf(" ");
+            }
+
+            for (int k = 1; k <= j; k++)
+            {
+                printf("C");
+            }
+        }
+        else
+        {
+            for (int q = 1; q < s; q++)
+            {
+                printf(" ");
+            }
+
+            s--;
+
+            for (int l = 1; l <= m; l++)
+            {
+                printf("C");
+            }
+
+            m++;
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
